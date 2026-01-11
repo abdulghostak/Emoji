11bd7e80888529c9ee2bc659092e40813e850673
abdulghostak <canawatermelon@gmail.com>
2026-01-11 18:03:35 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,27 +5,32 @@ const filesToCache = [
   '/manifest.json'
 ];
 
-// Install service worker
+// Install
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-    );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
 });
 
-// Activate service worker
+// Activate
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if (key !== cacheName) return caches.delete(key);
-            })
-        ))
-    );
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-// Fetch cached files
+// Fetch
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
